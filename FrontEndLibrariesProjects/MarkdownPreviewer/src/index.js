5d75093e3a3e7805bb51cca2762c6279948b0877
Richard Perrotta <perrottarichard@gmail.com>
2020-05-07 15:56:14 +0700

    updates

--- FrontEndLibrariesProjects/MarkdownPreviewer/src/index.js
+++ FrontEndLibrariesProjects/MarkdownPreviewer/src/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'React'
-import React from 'ReactDOM'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import marked from 'marked';
 import './custom.css'
 
 
@@ -10,20 +11,17 @@ class App extends Component {
                 <div class="container" id="bootstrap-override">
                     <div>
                         <h1>Give it a Try!</h1></div>
-
                     <Displayed />
-
                 </div>
             </div>
         );
     }
 }
-
 class Displayed extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            input: '# Type in this box to preview your markdown!\n## This is an example of some markdown text\n### Try to make your own using this previewer.\nYou can add some code using backticks, `<div></div>`.\n\n*You can write a multiline code block:*\n```\nfunction test(){\n\nconsole.log("Test");\n\n}\n\n```\n\nYou can list items like this:\n- Genre\n - Jazz\n - Blues\n - Classical\n\nYou can make your text **bold**, or \n> add a blockquote\n### Create links [FreeCodeCamp](https://www.freecodecamp.org/perrottarichard)...or even embed a photo of yourself!\n![pic](https://lh3.googleusercontent.com/JCUoAAxgaNtYAb4LvZVliny2VelZesumJo5ezXvyF5AZnG9gJoR03CK0MWxIx2ukvYzJ=s85)'
+            input: '# Type in this box to preview your markdown!\n## This is an example of some markdown text\n### Try to make your own using this previewer.\nYou can add some code using backticks, \n\n`<div></div>`\n`console.log("Hello")`\n\n\nYou can list items like this:\n- Genre\n - Jazz\n - Blues\n - Classical\n\nYou can make your text **bold**, or \n> add a blockquote\n### Create links [FreeCodeCamp](https://www.freecodecamp.org/perrottarichard)...or even embed a photo of Miles Davis!\n![pic](https://lh3.googleusercontent.com/proxy/T-A2ZMQRELiIIGZfAbaBIxD47JunVaceqPbhvhxsFg7BMotOcqyuj5ZlLuniEG_L2x_b_o0ifCK7nHj6yBcYKjH9JB1QuQBeV_5Od76lVPIS70W-I5rkhV3Xpv0h6WPhNbN1V9vk6GQjaOMZJwSdTl84pnJ0P0jsn4M9Yrg7qc_gKwSAUbM)'
         }
         this.handleChange = this.handleChange.bind(this)
     }
@@ -33,10 +31,6 @@ class Displayed extends Component {
     }
 
     render() {
-        function createMarkup() {
-            let inp = marked(this.state.input);
-            return { __html: inp };
-        }
         return (
             <div className="row">
 
@@ -48,6 +42,5 @@ class Displayed extends Component {
         )
     }
 }
-
 ReactDOM.render(
     <App />, document.getElementById('root'));
--- FrontEndLibrariesProjects/PomodoroClock/pomodoro-clock/src/store.js
+++ FrontEndLibrariesProjects/PomodoroClock/pomodoro-clock/src/store.js
@@ -1,14 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import reducer from "./reducers";
 
-const middleware = [thunk];
+
 const store = createStore(
   reducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
 );
 
 export default store;
