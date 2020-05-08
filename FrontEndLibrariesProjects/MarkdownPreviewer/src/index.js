a93c6be188608c79d1d554b65b268e6154167ba2
Richard Perrotta <perrottarichard@gmail.com>
2020-05-08 11:39:22 +0700

    updates

--- FrontEndLibrariesProjects/MarkdownPreviewer/src/index.js
+++ FrontEndLibrariesProjects/MarkdownPreviewer/src/index.js
@@ -21,7 +21,7 @@ class Displayed extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            input: '# Type in this box to preview your markdown!\n## This is an example of some markdown text\n### Try to make your own using this previewer.\nYou can add some code using backticks, \n\n`<div></div>`\n`console.log("Hello")`\n\n\nYou can list items like this:\n- Genre\n - Jazz\n - Blues\n - Classical\n\nYou can make your text **bold**, or \n> add a blockquote\n### Create links [FreeCodeCamp](https://www.freecodecamp.org/perrottarichard)...or even embed a photo of Miles Davis!\n![pic](https://lh3.googleusercontent.com/proxy/T-A2ZMQRELiIIGZfAbaBIxD47JunVaceqPbhvhxsFg7BMotOcqyuj5ZlLuniEG_L2x_b_o0ifCK7nHj6yBcYKjH9JB1QuQBeV_5Od76lVPIS70W-I5rkhV3Xpv0h6WPhNbN1V9vk6GQjaOMZJwSdTl84pnJ0P0jsn4M9Yrg7qc_gKwSAUbM)'
+            input: '# Type in this box to preview your markdown!\n## This is an example of some markdown text\n### Try to make your own using this previewer.\nYou can add some code using backticks, \n\n`<div></div>`\n`console.log("Hello")`\n\n\nYou can list items like this:\n- Genre\n - Jazz\n - Blues\n - Classical\n\nYou can make your text **bold**, or \n> add a blockquote\n### Create links [FreeCodeCamp](https://www.freecodecamp.org/perrottarichard)...or even embed a photo of Miles Davis!\n![pic](https://i.guim.co.uk/img/media/1b49bef060b31d5ec2d7b3d2a0500e412cb5d312/0_64_3713_2228/500.jpg?quality=85&auto=format&fit=max&s=1e7e534a8d0bff4c583579031dafa8e7)'
         }
         this.handleChange = this.handleChange.bind(this)
     }
