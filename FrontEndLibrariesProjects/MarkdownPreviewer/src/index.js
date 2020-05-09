4711734fdda3df18b9f18a10a6654a85f0e9d244
Richard Perrotta <perrottarichard@gmail.com>
2020-05-09 23:58:28 +0700

    update

--- FrontEndLibrariesProjects/MarkdownPreviewer/src/index.js
+++ FrontEndLibrariesProjects/MarkdownPreviewer/src/index.js
@@ -8,7 +8,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div class="container" id="bootstrap-override">
+                <div className="container" id="bootstrap-override">
                     <div>
                         <h1>Give it a Try!</h1></div>
                     <Displayed />
@@ -21,7 +21,7 @@ class Displayed extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            input: '# Type in this box to preview your markdown!\n## This is an example of some markdown text\n### Try to make your own using this previewer.\nYou can add some code using backticks, \n\n`<div></div>`\n`console.log("Hello")`\n\n\nYou can list items like this:\n- Genre\n - Jazz\n - Blues\n - Classical\n\nYou can make your text **bold**, or \n> add a blockquote\n### Create links [FreeCodeCamp](https://www.freecodecamp.org/perrottarichard)...or even embed a photo of Miles Davis!\n![pic](https://i.guim.co.uk/img/media/1b49bef060b31d5ec2d7b3d2a0500e412cb5d312/0_64_3713_2228/500.jpg?quality=85&auto=format&fit=max&s=1e7e534a8d0bff4c583579031dafa8e7)'
+            input: '# Type in this box to preview your markdown!\n## This is an example of some markdown text\n### Try to make your own using this previewer.\nYou can add some code using backticks, \n\n`<div></div>`\n`console.log("Hello")`\n\n\nYou can list items like this:\n- Genre\n - Jazz\n - Blues\n - Classical\n\nYou can make your text **bold**, or \n> add a blockquote\n### Create links [FreeCodeCamp](https://www.freecodecamp.org/perrottarichard)...or even embed a photo of Miles Davis!\n![pic](https://m.media-amazon.com/images/I/518js3KqN9L._AA256_.jpg)'
         }
         this.handleChange = this.handleChange.bind(this)
     }
